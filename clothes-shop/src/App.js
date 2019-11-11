40acaca3f1187d3405e14f2bdb06074bb40d5fa9
Jacques <jacknalletamby@gmail.com>
2019-11-11 20:26:39 +0100

    sass added

---
+++ clothes-shop/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './homepage.component';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+        <HomePage />
+    </div>
+  );
+}
+
+export default App;

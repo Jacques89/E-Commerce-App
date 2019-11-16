791e09f5534f26e530e3b856488d0eded9a2143c
Jacques <jacknalletamby@gmail.com>
2019-11-16 14:59:34 +0100

    updated shop data with new ID's, new utils added for cart

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button';
+import { addItem } from '../../redux/cart/cart-actions';
 
 import './collection-item.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className='collection-item'>
-        <div className='image' 
-            style={{backgroundImage: `url(${imageUrl})`}} 
-        />
+const CollectionItem = ({item, addItem}) => {
+    const {name, price, imageUrl } = item;
+
+    return(
+        <div className='collection-item'>
+            <div className='image' 
+                style={{backgroundImage: `url(${imageUrl})`}} 
+            />
             <div className='footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-    </div>
-);
+            <CustomButton 
+                onClick={() => addItem(item)} 
+                inverted>
+                ADD TO CART
+            </CustomButton>
+        </div>
+    )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);

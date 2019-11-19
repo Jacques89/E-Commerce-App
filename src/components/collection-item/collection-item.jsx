952a69e0dc1218d1464b0edc38dee2770b37943f
Jacques <jacknalletamby@gmail.com>
2019-11-19 16:38:55 +0100

    moved data and directory into respective reducers, created selectors and updated affected components

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -16,7 +16,7 @@ const CollectionItem = ({item, addItem}) => {
             />
             <div className='footer'>
                 <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
+                <span className='price'>{price}€</span>
             </div>
             <CustomButton 
                 onClick={() => addItem(item)} 
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
-import Collection from '../../components/collection/collection';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const {collections} = this.state
-        return(
-            <div className='shop-page'>
-                {collections.map(({id, ...otherCollectionProps}) => (
-                    <Collection key={id}
-                    {...otherCollectionProps} />
-                ))}
-            </div>
-        );
-    }
-}
+const ShopPage = ({ match }) => {
+    return(
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview} /> 
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        </div>
+    );
+};
 
 export default ShopPage;

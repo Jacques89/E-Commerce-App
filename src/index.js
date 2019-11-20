19e2aa19055e5565eebc2f1d5cf8b99dc31797f8
Jacques <jacknalletamby@gmail.com>
2019-11-20 17:35:09 +0100

    added spinner for data loading

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import {store, persistor} from './redux/store';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(

f1420fae0f93040d54081757845b9f17149e4299
Jacques <jacknalletamby@gmail.com>
2019-11-28 13:29:23 +0100

    renamed files for clarification, added Sagas for emailSignIn

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { CartItemContainer, ItemDetailsContainer, CartItemImage } from './cart-item.styles';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+    <CartItemContainer>
+        <CartItemImage src={imageUrl} alt='item' />
+        <ItemDetailsContainer>
+            <span>{name}</span>
+            <span>
+            {quantity} x {price}€
+            </span>
+        </ItemDetailsContainer>
+    </CartItemContainer>
+);
+
+export default CartItem;

f6b7311b1d2bc8f5545c6724b23aaf72cb4a2e75
Jacques <jacknalletamby@gmail.com>
2019-12-26 22:27:39 +0000

    added compression to server.js

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    app.use('compression');
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
    
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 

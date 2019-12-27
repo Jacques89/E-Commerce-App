d98041ee3ca48fbb615d17c718a9ca3ab493d953
Jacques <jacknalletamby@gmail.com>
2019-12-27 15:46:18 +0000

    removed compression

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const enforce = require('express-sslify');
-const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -17,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(compression);
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
    

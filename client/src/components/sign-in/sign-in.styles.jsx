d4bb3e5aba9f495d0258aa337b9658bc5a7cd5b9
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-02-03 21:43:40 +0100

    added responsiveness to all containers and applicable CSS in JS

--- client/src/components/sign-in/sign-in.styles.jsx
+++ client/src/components/sign-in/sign-in.styles.jsx
@@ -4,6 +4,9 @@ export const SignInContainer = styled.div`
     width: 380px;
     display: flex;
     flex-direction: column;
+    @media screen and (max-width: 800px) {
+        width: 100%;
+    }
 `;
 
 export const SignInTitle = styled.h2`

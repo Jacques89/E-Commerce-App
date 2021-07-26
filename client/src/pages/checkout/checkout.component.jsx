f726d91a93990808dca10b45afbb5385cfa25d2e
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-07-26 19:06:21 +0200

    Changed card details and updated packages

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -42,7 +42,7 @@ export const CheckoutPage = ({ cartItems, total }) => (
         <WarningContainer>
             *Please use the following test credit card for payments*
             <br />
-            4242 4242 4242 4242 - Exp: 06/20 - CVV: 123
+            4242 4242 4242 4242 - Exp: 06/26 - CVV: 123
         </WarningContainer>
         <StripeCheckoutButton price={total} />
     </CheckoutPageContainer>

8fd570b80f407a9d87387a630c0ed4c1ab265f30
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-08-01 17:08:25 +0200

    Upgraded packages as best as possible, modified mobile view for collections pages

--- client/src/components/collection-item/collection-item.styles.jsx
+++ client/src/components/collection-item/collection-item.styles.jsx
@@ -1,90 +1,103 @@
-import styled from 'styled-components';
-import CustomButton from '../custom-button/custom-button.component';
+import styled from "styled-components";
+import CustomButton from "../custom-button/custom-button.component";
 
 export const CollectionItemContainer = styled.div`
-    width: 22vw;
+  width: 22vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  align-items: center;
+  position: relative;
+
+  &:hover {
+    .image {
+      opacity: 0.8;
+    }
+
+    button {
+      opacity: 0.85;
+      display: flex;
+    }
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 40vw;
+    grid-template-columns: 1fr;
     display: flex;
-    flex-direction: column;
-    height: 350px;
-    align-items: center;
-    position: relative;
+    justify-content: space-between;
 
     &:hover {
-        .image {
-            opacity: 0.8;
-        }
+      .image {
+        opacity: unset;
+      }
 
-        button {
-            opacity: 0.85;
-            display: flex;
-        }
+      button {
+        opacity: unset;
+      }
     }
-
-    @media screen and (max-width: 800px) {
-        width: 40vw;
-        grid-template-columns: 1fr 1fr;
-        display: flex;
-        justify-content: space-between;
-
-        &:hover {
-            .image {
-                opacity: unset;
-            }
-    
-            button {
-                opacity: unset;
-            }
-        }
-    }
+  }
 `;
 
 export const AddButton = styled(CustomButton)`
-    width: 80%;
-    opacity: 0.7;
-    position: absolute;
-    top: 255px;
-    display: none;
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 255px;
+  display: none;
 
-    @media screen and (max-width: 800px) {
-        display: block;
-        opacity: 0.9;
-        min-width: unset;
-        padding: 0 10px 0 10px;
-    }
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    min-width: unset;
+    top: 219px;
+    padding: 0 10px 0 10px;
+  }
 `;
 
-AddButton.displayName = 'AddButton';
+AddButton.displayName = "AddButton";
 
 export const BackgroundImage = styled.div`
-    width: 100%;
+  width: 100%;
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 5px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+
+  @media screen and (max-width: 800px) {
+    width: 80vw;
     height: 95%;
-    background-size: cover;
-    background-position: center;
-    margin-bottom: 5px;
-    background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+  }
 `;
 
-BackgroundImage.displayName = 'BackgroundImage';
+BackgroundImage.displayName = "BackgroundImage";
 
 export const CollectionFooterContainer = styled.div`
-    width: 100%;
-    height: 5%;
-    display: flex;
-    justify-content: space-between;
-    font-size: 18px;
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+
+  @media screen and (max-width: 800px) {
+    width: 80vw;
+    height: 20%;
+    font-size: 36px;
+  }
+
 `;
 
-CollectionFooterContainer.displayName = 'CollectionFooterContainer';
+CollectionFooterContainer.displayName = "CollectionFooterContainer";
 
 export const NameContainer = styled.span`
-    width: 90%;
-    margin-bottom: 15px;
+  width: 90%;
+  margin-bottom: 15px;
 `;
 
-NameContainer.displayName = 'NameContainer';
+NameContainer.displayName = "NameContainer";
 
 export const PriceContainer = styled.span`
-    text-align: right;
+  text-align: right;
 `;
 
-PriceContainer.displayName = 'PriceContainer';
+PriceContainer.displayName = "PriceContainer";

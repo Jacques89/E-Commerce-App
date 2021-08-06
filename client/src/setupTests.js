7840d52f2f6baf79dd7d5e393bf0d5b550801fe5
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-08-06 19:49:11 +0200

    Updated React package and enzyme adaptor

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,4 +1,6 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import '@testing-library/jest-dom'
 
-Enzyme.configure({ adapter: new Adapter() })
+import { configure } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+configure({ adapter: new Adapter() })

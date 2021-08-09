9f8a211bfc2d839320b562f04330af28ccc2afde
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-08-09 15:06:21 +0200

    Added contact form

---
+++ client/src/pages/contact/contact.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContactForm from '../../components/contact-form/contact-form.component'
+import { FormPageContainer } from './contact.styles'
+
+const ContactPage = () => (
+    <FormPageContainer>
+        <ContactForm />
+    </FormPageContainer>
+);
+
+export default ContactPage;
